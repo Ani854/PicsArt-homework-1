bcf51bca7ca3c2ba7409512974f376b2923bf999
Ani Stepanyan <masriam.stepanyan@beeoncode.com>
2021-03-26 21:13:30 +0400

    first commit

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+public class Test {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        System.out.println("b = " + b);
+
+        short s = 458;
+        System.out.println("s = " + s);
+
+        int i = 1000;
+        System.out.println("i = " + i);
+
+        long l = 143657L;
+        System.out.println("l = " + l);
+
+        double d = 12.5;
+        System.out.println("d = " + d);
+
+        float f = 2563.32F;
+        System.out.println("f = " + f);
+
+        boolean b1 = true;
+        System.out.println("b1 - " + b1);
+
+        boolean b2 = false;
+        System.out.println("b2 - " + b2);
+
+        char ch = '#';
+        System.out.println("ch = " + ch);
+    }
+}
